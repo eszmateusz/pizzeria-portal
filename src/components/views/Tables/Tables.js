9db45c0ce9ce4b9df8fe4baaa3ca9fa8240fe54c
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-03-28 21:49:13 +0200

    Fix func to change status of order. Add scheduler to tables view. Add appointments to calendar. Not working correctly.

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -11,17 +11,54 @@ import TablesBookingId from '../TablesBookingId/TablesBookingId';
 import TablesEventsNew from '../TablesEventsNew/TablesEventsNew';
 import TablesEventsId from '../TablesEventsId/TablesEventsId';
 
+import Paper from '@material-ui/core/Paper';
+import { ViewState }  from '@devexpress/dx-react-scheduler';
+import { 
+  Scheduler, 
+  DayView, 
+  Appointments, 
+  AppointmentTooltip, 
+  DateNavigator, 
+  TodayButton, 
+  Toolbar,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const data = [
+  { startDate: '2021-03-29 14:00', endDate: '2021-03-29 16:30', title: 'Alex Portney, table 1' },
+  { startDate: '2021-03-29 18:00', endDate: '2021-03-29 19:30', title: 'Tomas Kirby, table 2' },
+  { startDate: '2021-03-29 14:00', endDate: '2021-03-29 16:00', title: 'Blanka Kerth, table 3' },
+  { startDate: '2021-03-29 15:00', endDate: '2021-03-29 17:30', title: 'Katy Simpson, table 2' },
+  { startDate: '2021-03-29 19:00', endDate: '2021-03-29 20:30', title: 'John Tatcher, table 1' },
+];
+
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables</h2>
     <hr />
-    <h3>Bookings</h3>
+    <h3>Bookings and events</h3>
+
+    <Paper>
+      <Scheduler
+        data={data}
+      >
+        <ViewState
+          defaultCurrentDate={new Date()}/>
+        <DayView
+          startDayHour={12}
+          endDayHour={24}
+        />
+        <Toolbar />
+        <DateNavigator />
+        <TodayButton />
+        <Appointments />
+        <AppointmentTooltip />
+      </Scheduler>
+    </Paper>
+
     <Link to={`${process.env.PUBLIC_URL}/tables/booking/as897`}>Booking info</Link>
     <Fab color='secondary' aria-label='add' component={Link} to={`${process.env.PUBLIC_URL}/tables/booking/new`}>
       <AddIcon />
     </Fab>
-    <hr />
-    <h3>Events</h3>
     <Link to={`${process.env.PUBLIC_URL}/tables/events/df14`}>Events info</Link>
     <Fab color='secondary' aria-label='add' component={Link} to={`${process.env.PUBLIC_URL}/tables/events/new`}>
       <AddIcon />
